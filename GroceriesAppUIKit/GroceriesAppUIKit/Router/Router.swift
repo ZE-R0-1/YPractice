71f5cffaf8a6a80584a85faabac46db56c8887e0
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2024-07-12 15:33:16 +0900

    Initial commit

---
+++ GroceriesAppUIKit/GroceriesAppUIKit/Router/Router.swift
@@ -0,0 +1,29 @@
+//
+//  Router.swift
+//  GroceriesApp
+//
+//  Created by Everton Carneiro on 01/09/23.
+//
+
+import UIKit
+
+final class Router {
+    let navigation: UINavigationController
+        
+    init(navigation: UINavigationController) {
+        self.navigation = navigation
+    }
+    
+    private func push(controller: UIViewController) {
+        navigation.pushViewController(controller, animated: true)
+    }
+    
+    private func pop() {
+        navigation.popViewController(animated: true)
+    }
+    
+    private func pushCategoriesViewController() {
+        let controller = CategoriesViewController(router: self)
+        push(controller: controller)
+    }
+}
---
+++ GroceriesAppUIKit/GroceriesAppUIKit/View/Controllers/CategoriesViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CategoriesViewController.swift
+//  GroceriesAppUIKit
+//
+//  Created by USER on 7/12/24.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+    
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
